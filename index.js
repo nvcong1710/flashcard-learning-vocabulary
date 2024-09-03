f5b68c81a69a1198c779cdaad3b1c75244e3aa3c
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-09-03 18:55:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const dataFile = "data.json";
+
+async function fetchData() {
+  try {
+    const response = await fetch(dataFile);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error loading data:", error);
+    return [];
+  }
+}
+
+async function displayRandomWord() {
+  const data = await fetchData();
+
+  if (data.length === 0) {
+    document.getElementById("word").innerText = "Không có dữ liệu từ vựng.";
+    document.getElementById("meaning").innerText = "";
+    document.getElementById("example").innerText = "";
+    return;
+  }
+
+  const cardInner = document.querySelector(".card-inner");
+  if (cardInner.classList.contains("flipped")) {
+    cardInner.classList.remove("flipped");
+  }
+
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const randomWord = data[randomIndex];
+  document.getElementById("word").innerText = randomWord.word;
+  document.getElementById("meaning").innerText = randomWord.meaning;
+  document.getElementById("example").innerText = randomWord.example;
+}
+
+function toggleFlip() {
+  document.querySelector(".card-inner").classList.toggle("flipped");
+}
+
+document.addEventListener("DOMContentLoaded", displayRandomWord);
