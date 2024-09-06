4d315e553a8573c42832c80df1add553c8269cd5
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-09-06 22:13:22 +0700

    clean code

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// async function fetchData() {
-//   try {
-//     const storedData = localStorage.getItem("vocabularyData");
-//     if (storedData) {
-//       return JSON.parse(storedData);
-//     }
-//   } catch (error) {
-//     console.error("Error loading data:", error);
-//     return [];
-//   }
-// }
-
 async function fetchData() {
   try {
     const storedData = localStorage.getItem("vocabularyData");
@@ -27,28 +15,6 @@ async function fetchData() {
     return [];
   }
 }
-
-// async function displayRandomWord() {
-//   const data = await fetchData();
-
-//   if (data.length === 0) {
-//     document.getElementById("word").innerText = "Không có dữ liệu từ vựng.";
-//     document.getElementById("meaning").innerText = "";
-//     document.getElementById("example").innerText = "";
-//     return;
-//   }
-
-//   const cardInner = document.querySelector(".card-inner");
-//   if (cardInner.classList.contains("flipped")) {
-//     cardInner.classList.remove("flipped");
-//   }
-
-//   const randomIndex = Math.floor(Math.random() * data.length);
-//   const randomWord = data[randomIndex];
-//   document.getElementById("word").innerText = randomWord.word;
-//   document.getElementById("meaning").innerText = randomWord.meaning;
-//   document.getElementById("example").innerText = randomWord.example;
-// }
 
 async function displayRandomWord() {
   const data = await fetchData();
